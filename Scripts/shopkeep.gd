f6705699d4ba2901c2d034d9b6a3898e6a8a6a1d
John Bickel <bickel.john@yahoo.com>
2023-04-12 10:50:51 -0400

    Sprite fix
    Player sprite now goes to idle animation when interacting w/ shopkeeper.

--- Scripts/shopkeep.gd
+++ Scripts/shopkeep.gd
@@ -4,7 +4,7 @@ extends Area2D
 func _on_body_entered(body):
 	if body.name == "Player":
 		get_tree().paused = true
-		#get_node("res://Scenes/Characters/Player.tscn").play("Idle")
+		get_node("../Player/AnimatedSprite2D").play("Idle")
 		get_node("Shop/Anim").play("TransIn")
 
 #func purchase_upgrade(multiplier):
