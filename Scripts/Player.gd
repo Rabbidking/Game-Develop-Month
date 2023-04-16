88e5311c4e828e9bccf74416a26b2b4f500f7003
John Bickel <bickel.john@yahoo.com>
2023-04-15 22:12:43 -0400

    Update Player.gd
    Updated player script with Brandon Li's changes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,8 @@ var SPEED = 400.0
 var JUMP_VELOCITY = -500.0
 var coin_manager = preload("res://Scripts/CoinManager.gd")
 var damage_multiplier = 1
+var attacking = false
+var jumping = false
 #var jump_max = 2
 #var jump_count = 0
 #const SPEED = 300.0
@@ -33,9 +35,11 @@ func _physics_process(delta):
 #			velocity.y = JUMP_VELOCITY
 #			jump_count += 1
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and attacking == false:
+		jumping = true
+		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.play("Jump")
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,14 +49,18 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-		
-	if Input.is_action_just_pressed("ui_left") and is_on_floor():
-		$AnimatedSprite2D.play("Walk")
-		$AnimatedSprite2D.flip_h = true
-	elif Input.is_action_just_pressed("ui_right") and is_on_floor():
+	if velocity.x > 0 and is_on_floor() and attacking == false and jumping == false:
 		$AnimatedSprite2D.play("Walk")
 		$AnimatedSprite2D.flip_h = false
-	elif Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right") and is_on_floor():
+		$HitBox/CollisionShape2D.position = Vector2(-4, 41)
+	elif velocity.x < 0 and is_on_floor() and attacking == false and jumping == false:
+		$AnimatedSprite2D.play("Walk")
+		$AnimatedSprite2D.flip_h = true
+		$HitBox/CollisionShape2D.position = Vector2(-139, 41)
+	elif velocity.y > 0 and not is_on_floor() and attacking == false and jumping == false:
+		$AnimatedSprite2D.play("Falling")
+#	elif Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right") and is_on_floor():
+	elif velocity.x == 0 and velocity.y == 0 and is_on_floor() and attacking == false and jumping == false:
 		$AnimatedSprite2D.play("Idle")
 
 	move_and_slide()
@@ -75,6 +83,7 @@ func add_coin():
 #			JUMP_VELOCITY = 100
 
 func throw_coin():
+	attacking = true
 	$AnimatedSprite2D.play("Toss")
 	# default is Z
 	if Game.coins <= 0:
@@ -85,11 +94,14 @@ func throw_coin():
 	print("Num coins: " + str(Game.coins))
 	
 func smack():
+	attacking = true
 	$AnimatedSprite2D.play("Smack")
 	$HitBox/CollisionShape2D.disabled = false
 
 func _on_animated_sprite_2d_animation_finished():
 	$AnimatedSprite2D.play("Idle")
+	jumping = false
+	attacking = false
 	$HitBox/CollisionShape2D.disabled = true
 
 func _on_hit_box_body_entered(body):
