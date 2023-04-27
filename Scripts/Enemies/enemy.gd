69ea5abd149cfa928d45b0dd4f5e2eb9fcf577ef
John Bickel <bickel.john@yahoo.com>
2023-04-27 00:24:00 -0400

    Enemy script more generalized
    Slime is now in line with generic enemy script, so now all enemies can share this script with minimal changes to attack behavior.

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 @export var health = 10
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+
+@onready var anim = $AnimatedSprite2D
+#const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 
 func _physics_process(delta):
@@ -19,9 +22,10 @@ func hurt():
 		die()
 
 func die():
-	$AnimatedSprite2D_Alive.visible = false
-	$AnimatedSprite2D_Death.visible = true
-	$AnimatedSprite2D_Death.play("Death")
-
-func _on_animated_sprite_2d_death_animation_finished():
+	#$AnimatedSprite2D_Alive.visible = false
+	#$AnimatedSprite2D_Death.visible = true
+	#$AnimatedSprite2D_Death.play("Death")
+	anim.play("die")
+	await anim.animation_finished
 	queue_free()
+	
