aa0e5614481646310b909b33602bbfd0bfb46e3f
John Bickel <bickel.john@yahoo.com>
2023-04-29 13:24:33 -0400

    Fixed collision layers with floor tilemaps

--- Scripts/level_exit.gd
+++ Scripts/level_exit.gd
@@ -3,5 +3,5 @@ extends Area2D
 func _on_body_entered(body):
 	if body.name == "Player":
 		get_tree().paused = true
-		get_node("../Player/AnimatedSprite2D").play("Idle")
+		#get_node("../Player/AnimatedSprite2D").play("Idle")
 		get_node("LevelSelect/Anim").play("TransIn")
